fa3eb85a6175a8b3c6db196c08f1545c8a383621
Andrei MARDALE <mrdlandrei@gmail.com>
2022-01-25 19:54:44 +0200

    Add implementation that avoids max-recursion errors

--- flatten.js
+++ flatten.js
@@ -8,6 +8,7 @@
  * @param {*} acc internal value used for tail call optimization
  * @returns flattened array
  */
+// eslint-disable-next-line no-unused-vars
 function flattenHelper(arr, acc = null) {
   const res = acc !== null ? acc : [];
 
@@ -22,12 +23,53 @@ function flattenHelper(arr, acc = null) {
   return res;
 }
 
+/**
+ * More complicated implementation that avoids max recursion depth errors
+ * This explicitly uses a stack instead of relying on language-level recursion
+ * @param {*} arr - maybe nested array containing any type of elements
+ * @param {*} acc internal value used for tail call optimization
+ * @returns flattened array
+ */
+function flattenHelperIterative(initialArray) {
+  const res = [];
+
+  const processStack = [{ array: initialArray, idx: 0 }];
+
+  while (processStack.length > 0) {
+    const { array, idx } = processStack.pop();
+
+    for (let i = idx; i < array.length; i += 1) {
+      const elem = array[i];
+
+      if (Array.isArray(elem)) {
+        /**
+         * sub-array encountered
+         * We have to process this sub-array first and then resume from the next index
+         */
+
+        // mark that we must continue from the next index for the current array
+        processStack.push({ array, idx: i + 1 });
+
+        // but first process the sub-array
+        processStack.push({ array: elem, idx: 0 });
+
+        // break the current loop - will be resumed by a future stack pop operation
+        break;
+      } else {
+        res.push(array[i]);
+      }
+    }
+  }
+
+  return res;
+}
+
 function flatten(arr) {
   if (!Array.isArray(arr)) {
     throw new TypeError('top-level value should be an array');
   }
 
-  return flattenHelper(arr);
+  return flattenHelperIterative(arr);
 }
 
 module.exports = flatten;
